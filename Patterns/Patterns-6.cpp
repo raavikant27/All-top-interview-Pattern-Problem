46cf4d4701ae0004956baacdd6b5dfa34ab9cb15
Ravikant singh <sravikant302@gmail.com>
2025-08-07 22:44:47 +0530

    pattern-6,7,8,9

---
+++ Patterns/Patterns-6.cpp
@@ -0,0 +1,24 @@
+// *****
+
+// ****
+
+// ***
+
+// **
+
+// *
+//dry-run the code -
+
+class Solution {
+public:
+    void pattern5(int n) {
+
+        for(int i=1;i<=n;i++){
+            for( int j=1;j<=n-i+1;j++){
+                cout<< "*";
+            }
+            cout<<endl;
+        }
+
+    }
+};
